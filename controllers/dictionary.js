eaf861c535f7bdad6eda0901caff6eca29572006
Roman-S10 <blizomir@yandex.ru>
2020-10-07 22:26:48 +0300

    adjusted dictionary methods

--- controllers/dictionary.js
+++ controllers/dictionary.js
@@ -3,33 +3,37 @@ const path = require('path');
 const os = require('os');
 const lineReader = require('line-reader');
 
-const __getDictionaryFilePath = () => {
+const _getDictionaryFilePath = () => {
     const root = path.dirname(require.main.filename || process.require.main.filename);
     return path.join(root, 'dictionary.txt');
 }
 
+const _prepearString = (str) => {
+    return str.trim().toLowerCase();
+}
+
 exports.createWordsPair = (req, res) => {
     const {word, translation} = req.body;
-    const pair = `${word.toLowerCase()}=${translation.toLowerCase()}${os.EOL}`;
-    
-    fs.appendFile(__getDictionaryFilePath(), pair, err => {
+    const pair = `${_prepearString(word)}=${_prepearString(translation)}${os.EOL}`;
+
+    fs.appendFile(_getDictionaryFilePath(), pair, err => {
         if(err) throw err;
-        res.send(`New words pair [${pair}] added to the file.`);
+        res.send({info: `New words pair [ ${pair} ] added to the file.`});
     })
 };
 
 exports.getTranslation = (req, res) => {
     
-    lineReader.eachLine(__getDictionaryFilePath(), line => {
-        const word = req.params.word.toLowerCase();
+    lineReader.eachLine(_getDictionaryFilePath(), (line, last) => {
+        const word = _prepearString(req.params.word);
 
         if(line.includes(word)) {
             const index = line.indexOf('=');
             const translation = line.substr(index + 1);
-            res.send({"translation": translation});
+            res.send({translation: translation});
             return false;
-        }else{
-            res.send(`Sorry, there is no any translation for [${word}].`);
+        }else if(last){
+            res.send({error: `Sorry, there is no any translation for [ ${word} ].`});
             return false;
         }
     });
