5018872a8f24d5c1588bb1f900aa1d391fadc48f
Roman-S10 <blizomir@yandex.ru>
2020-10-07 22:40:45 +0300

    tab fixed

--- client/webapp/controller/Tab.controller.js
+++ client/webapp/controller/Tab.controller.js
@@ -4,24 +4,34 @@ sap.ui.define([
    "use strict";
    return Controller.extend("sap.ui.client.controller.Tab", {
       onAddTranslation : function () {
-         const word = this.getView().byId("input-word").getValue();
-         const translation = this.getView().byId("input-translation").getValue();
-         fetch('/', {
+         const inputWord = this.getView().byId("input-word");
+         const word = inputWord.getValue();
+         const inputTranslation = this.getView().byId("input-translation");
+         const translation = inputTranslation.getValue();
+         const infoText = this.getView().byId("info-text");
+
+         fetch('http://localhost:3000/', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify({
-               "word":word,
+               "word": word,
                "translation": translation
             })
-          });
+         })
+         .then(response => response.json())
+         .then(json => infoText.setHtmlText(json.info));
+
+         inputTranslation.setValue('');
+         inputWord.setValue('');
       },
       onGetTranslation : function () {
          const word = this.getView().byId("input-word-for-translation").getValue();
-         fetch(`/${word}`)
+         const translationText = this.getView().byId("translation-text");
+         fetch(`http://localhost:3000/${word}`)
          .then(response => response.json())
-         .then(json => console.log(json))
+         .then(json => translationText.setHtmlText(json.translation ? json.translation : json.error));
       }
    });
 });
